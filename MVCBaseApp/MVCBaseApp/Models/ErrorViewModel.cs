259aac92cbe120fc828ceda4054f8504e6795742
Zehua RONG <rhican.r@gmail.com>
2019-07-22 17:04:06 +0800

    init

---
+++ MVCBaseApp/MVCBaseApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCBaseApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
